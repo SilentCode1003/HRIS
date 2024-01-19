a47938cf348e96ec56107b8d35836ef1a930461e
blancowendell <blanco.wendell15@gmail.com>
2024-01-19 16:44:29 +0800

    revised attendance 2024/01/09

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,56 +1,58 @@
-const mysql = require('./repository/hrmisdb');
-const moment = require('moment');
-var express = require('express');
-const { Validator } = require('./controller/middleware');
+const mysql = require("./repository/hrmisdb");
+const moment = require("moment");
+var express = require("express");
+const { Validator } = require("./controller/middleware");
+const e = require("express");
 var router = express.Router();
 const currentDate = moment();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   //res.render('attendancelayout', { title: 'Express' });
 
-  Validator(req, res, 'attendancelayout');
+  Validator(req, res, "attendancelayout");
 });
 
 module.exports = router;
 
-router.post('/set-geofence', async (req, res) => {
+router.post("/set-geofence", async (req, res) => {
   const { geofenceLatitude, geofenceLongitude, geofenceRadius } = req.body;
 
   try {
-      const GeoFenceSettings = mongoose.model('GeoFenceSettings', {
-          latitude: Number,
-          longitude: Number,
-          radius: Number,
+    const GeoFenceSettings = mongoose.model("GeoFenceSettings", {
+      latitude: Number,
+      longitude: Number,
+      radius: Number,
+    });
+
+    const existingSettings = await GeoFenceSettings.findOne();
+
+    if (existingSettings) {
+      existingSettings.latitude = geofenceLatitude;
+      existingSettings.longitude = geofenceLongitude;
+      existingSettings.radius = geofenceRadius;
+      await existingSettings.save();
+    } else {
+      const newSettings = new GeoFenceSettings({
+        latitude: geofenceLatitude,
+        longitude: geofenceLongitude,
+        radius: geofenceRadius,
       });
+      await newSettings.save();
+    }
 
-      const existingSettings = await GeoFenceSettings.findOne();
-
-      if (existingSettings) {
-          existingSettings.latitude = geofenceLatitude;
-          existingSettings.longitude = geofenceLongitude;
-          existingSettings.radius = geofenceRadius;
-          await existingSettings.save();
-      } else {
-          const newSettings = new GeoFenceSettings({
-              latitude: geofenceLatitude,
-              longitude: geofenceLongitude,
-              radius: geofenceRadius,
-          });
-          await newSettings.save();
-      }
-
-      res.status(200).json({ message: 'Geo-fence settings updated successfully' });
+    res
+      .status(200)
+      .json({ message: "Geo-fence settings updated successfully" });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 router.get("/load", (req, res) => {
-    try {
-        let sql =`SELECT
+  try {
+    let sql = `SELECT
         me_profile_pic as image,
         ma_attendanceid as attendanceid,
         CONCAT(me_lastname, " ", me_firstname) as employeeid,
@@ -67,35 +69,35 @@ router.get("/load", (req, res) => {
         LEFT JOIN master_employee ON ma_employeeid = me_id
         ORDER BY ma_attendancedate DESC`;
 
-        mysql.mysqlQueryPromise(sql)
-        .then((result) => {
-            res.json({
-                msg: 'success',
-                data: result,
-            });
-        })
-        .catch((error) => {
-            res.json({
-                msg:'error',
-                data: error,
-            });
-        })
-    } catch (error) {
-        console.log('error',error)
-    }
+    mysql
+      .mysqlQueryPromise(sql)
+      .then((result) => {
+        res.json({
+          msg: "success",
+          data: result,
+        });
+      })
+      .catch((error) => {
+        res.json({
+          msg: "error",
+          data: error,
+        });
+      });
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
-
 router.post("/getloadforapp", (req, res) => {
-    try {
-        let employeeid = req.body.employeeid;
-        let sql =`       
+  try {
+    let employeeid = req.body.employeeid;
+    let sql = `       
         SELECT
         CONCAT(me_lastname, " ", me_firstname) as employeeid,
-        ma_attendancedate as attendancedatein,
-        DATE_FORMAT(al_logdatetime, '%Y-%m-%d') as attendancedateout,
         TIME_FORMAT(ma_clockin, '%H:%i:%s') as clockin,
         TIME_FORMAT(ma_clockout, '%H:%i:%s') as clockout,
+        DATE_FORMAT(ma_clockout, '%Y-%m-%d') as attendancedateout,
+        DATE_FORMAT(ma_clockin, '%Y-%m-%d') as attendancedatein,
         ma_devicein as devicein,
         ma_deviceout as deviceout,
         CONCAT(
@@ -104,32 +106,32 @@ router.post("/getloadforapp", (req, res) => {
         ) AS totalhours
         FROM master_attendance
         INNER JOIN master_employee ON ma_employeeid = me_id
-        INNER JOIN attendance_logs ON al_attendanceid = ma_attendanceid AND al_logtype='ClockOut'
-        where ma_employeeid = '${employeeid}'
+        where ma_employeeid='${employeeid}'
         ORDER BY ma_attendancedate DESC`;
-  
-        mysql.mysqlQueryPromise(sql)
-        .then((result) => {
-            res.json({
-                msg: 'success',
-                data: result,
-            });
-        })
-        .catch((error) => {
-            res.json({
-                msg:'error',
-                data: error,
-            });
-        })
-    } catch (error) {
-        console.log('error',error)
-    }
+
+    mysql
+      .mysqlQueryPromise(sql)
+      .then((result) => {
+        res.json({
+          msg: "success",
+          data: result,
+        });
+      })
+      .catch((error) => {
+        res.json({
+          msg: "error",
+          data: error,
+        });
+      });
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
-router.post('/logs', (req, res) => {
-    try {
-        let attendanceid = req.body.attendanceid;
-        let sql =  `SELECT
+router.post("/logs", (req, res) => {
+  try {
+    let attendanceid = req.body.attendanceid;
+    let sql = `SELECT
         me_profile_pic AS image,
         CONCAT(me_lastname, ' ', me_firstname) AS fullname,
         DATE_FORMAT(al_logdatetime, '%W, %M %e, %Y') AS logdate,
@@ -148,37 +150,36 @@ router.post('/logs', (req, res) => {
         WHERE
         al_attendanceid = '${attendanceid}'`;
 
-        mysql.mysqlQueryPromise(sql)
-        .then((result) => {
-            res.json({
-                msg:'success',
-                data: result,
-            });
-        })
-        .catch((error) => {
-            res.json({
-                msg:error,
-            });
-        })
-    } catch (error) {
+    mysql
+      .mysqlQueryPromise(sql)
+      .then((result) => {
         res.json({
-            msg:error,
+          msg: "success",
+          data: result,
         });
-        
-    }
+      })
+      .catch((error) => {
+        res.json({
+          msg: error,
+        });
+      });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
 });
 
 // router.post('/gethomestatus', (req, res) => {
 //     try {
 //        //let attendancedate = req.body.attendancedate;
 //         let employeeid = req.body.employeeid;
-//         let sql = `SELECT 
+//         let sql = `SELECT
 //         DATE_FORMAT(ma_clockin, '%Y-%m-%d %H:%i:%s') AS formatted_clockin,
 //         DATE_FORMAT(ma_clockout, '%Y-%m-%d %H:%i:%s') AS formatted_clockout
 //         FROM master_attendance
 //         WHERE ma_employeeid = '${employeeid}'
 //         LIMIT 1`;
-      
 
 //         mysql.mysqlQueryPromise(sql)
 //         .then((result) => {
@@ -198,12 +199,11 @@ router.post('/logs', (req, res) => {
 //     }
 // });
 
-
-router.post('/gethomestatus2', (req, res) => {
-    try {
-        let employeeid = req.body.employeeid;
-        let attendancedate = req.body.attendancedate;
-        let sql = `select 
+router.post("/gethomestatus2", (req, res) => {
+  try {
+    let employeeid = req.body.employeeid;
+    let attendancedate = req.body.attendancedate;
+    let sql = `select 
 		DATE_FORMAT(ma_clockin, '%W, %M %e, %Y') AS logdatein,
         TIME(ma_clockin) AS logtimein,
         DATE_FORMAT(ma_clockout, '%W, %M %e, %Y') AS logdateout,
@@ -212,23 +212,51 @@ router.post('/gethomestatus2', (req, res) => {
         where ma_employeeid = '${employeeid}' and ma_attendancedate = '${attendancedate}'
         order by ma_attendancedate desc 
         limit 1`;
-        console.log(sql);
+    console.log(sql);
 
-        mysql.mysqlQueryPromise(sql)
-        .then((result) => {
-            res.json({
-                msg: 'success',
-                data: result,
-            });
-        })
-        .catch((error) => {
-            res.json({
-                msg: 'error',
-                data: error,
-            });
+    mysql
+      .mysqlQueryPromise(sql)
+      .then((result) => {
+        res.json({
+          msg: "success",
+          data: result,
         });
-    } catch (error) {
-        console.log('error', error);
-    }
+      })
+      .catch((error) => {
+        res.json({
+          msg: "error",
+          data: error,
+        });
+      });
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
+router.post('/filterforapp', (req, res) => {
+  try {
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate;
+    let sql = `SELECT ma_attendancedate as attendancedate
+    FROM master_attendance
+    WHERE ma_attendancedate BETWEEN '${startdate}' AND '${enddate}';`;
+
+    mysql.mysqlQueryPromise(sql)
+    .then((result) => {
+      res.json({
+        msg: 'success',
+        data: result,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        msg: 'error',
+        data: error,
+      });
+    })
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
