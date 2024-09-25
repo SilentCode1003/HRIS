4c333a5d3c564a8f032bf24a2517ede5d8f48c95
blancowendell <blanco.wendell15@gmail.com>
2024-09-25 16:57:16 +0800

    [FIX 404]

--- routes/repository/hrmisdb.js
+++ routes/repository/hrmisdb.js
@@ -10,7 +10,7 @@ Decrypter(process.env._PASSWORD_ADMIN, (err, encrypted) => {
   password = encrypted;
 });
 
-Decrypter("97b1658adae301177bba5f775972b020b58229f1876b445c56ed6d711ef4e376", (err, encrypted) => {
+Decrypter("172bcc47e08f5b7d18a612630a7299bc", (err, encrypted) => {
   if (err) console.error("Error: ", err);
   console.log(encrypted);
 });
