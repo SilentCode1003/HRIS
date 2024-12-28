e450bb6230fe5de83d669940c33bded08f7feebb
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2024-12-28 16:16:48 +0800

    Added View OB Activity on Admin

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { Decrypter, DecrypterString} = require('../routes/repository/cryptography');
 
 const verifyJWT = (req, res, next) => {
-    const token = req.session.jwt ?? req.body.APK;
+    const token = req.session.jwt ?? req.body.APK ?? req.query.APK;
 
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
