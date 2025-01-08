131065718929cf25b386822466ed96926124e631
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2025-01-08 15:04:34 +0800

    Added OB on mobile-api

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 const { Decrypter, DecrypterString} = require('../routes/repository/cryptography');
 
 const verifyJWT = (req, res, next) => {
-    const token = req.session.jwt ?? req.body.APK ?? req.query.APK;
-
+    const token = req.session.jwt ?? req.body.APK ?? req.query.APK ?? req.params.APK;
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
