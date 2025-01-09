8a47ff6a9ddddb6e1ca9379f9d0649df1ab8b54c
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2025-01-09 15:42:34 +0800

    Update Unauthorized Page

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -1,26 +1,31 @@
-const jwt = require('jsonwebtoken');
-const { Decrypter, DecrypterString} = require('../routes/repository/cryptography');
+const jwt = require("jsonwebtoken");
+const {
+  Decrypter,
+  DecrypterString,
+} = require("../routes/repository/cryptography");
+const { UnauthorizedTemplate } = require("../routes/repository/helper");
 
 const verifyJWT = (req, res, next) => {
-    const token = req.session.jwt ?? req.body.APK ?? req.query.APK ?? req.params.APK;
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
+  const token =
+    req.session.jwt ?? req.body.APK ?? req.query.APK ?? req.params.APK;
+  if (!token) {
+    return res.status(401).send(UnauthorizedTemplate());
+  }
 
-    Decrypter(token, (err, data) => {
+  Decrypter(token, (err, data) => {
+    if (err) {
+      return res.status(404).json({ msg: err });
+    } else {
+      jwt.verify(data, process.env._SECRET_KEY, (err, decoded) => {
         if (err) {
-            return res.status(404).json({ msg: err});
-        } else {
-            jwt.verify(data, process.env._SECRET_KEY, (err, decoded) => {
-                if (err) {
-                    console.log('JWT Error: ', err);
-                    return res.status(403).json({ msg: err});
-                }
-                req.user = decoded;
-                next();
-            });
+          console.log("JWT Error: ", err);
+          return res.status(403).json({ msg: err });
         }
-    });
+        req.user = decoded;
+        next();
+      });
+    }
+  });
 };
 
 module.exports = verifyJWT;
