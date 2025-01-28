a6a04fc37747b3ec9c5f6e672506eb48e5a58127
blancowendell <blanco.wendell15@gmail.com>
2025-01-28 14:53:43 +0800

    Fix migration

--- migrations/20250128013244-update-roa-total-hours.js
+++ migrations/20250128013244-update-roa-total-hours.js
@@ -4,20 +4,24 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
       UPDATE restday_ot_approval
-      SET roa_total_hours = NULL
-      WHERE roa_total_hours < 0;
+      SET roa_total_hours = 0; -- Using decimal value 0 which is compatible with DECIMAL
     `);
 
     await queryInterface.changeColumn('restday_ot_approval', 'roa_total_hours', {
       type: Sequelize.TIME,
-      allowNull: true, 
+      allowNull: true,
     });
+    
+    await queryInterface.sequelize.query(`
+      UPDATE restday_ot_approval
+      SET roa_total_hours = '00:00:00'; -- Update all values to '00:00:00' in TIME format
+    `);
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.changeColumn('restday_ot_approval', 'roa_total_hours', {
       type: Sequelize.DECIMAL(10, 5),
-      allowNull: true, 
+      allowNull: true,
     });
   },
 };
